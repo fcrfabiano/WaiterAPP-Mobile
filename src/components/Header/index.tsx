27ffd66307983fdcfac9b858ec257682827cd9f1
Fabiano César <fcfabiano.cesar@gmail.com>
2023-02-27 16:40:15 -0300

    feat: implement cart into header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,42 @@
+import { TouchableOpacity } from 'react-native';
 import { Text } from '../Text';
-import { Container } from './styles';
+import { Container, Content, OrderHeader, Table } from './styles';
 
-export function Header() {
+interface HeaderProps {
+  selectedTable: string;
+  onCancelOrder: () => void;
+}
+
+export function Header({ selectedTable, onCancelOrder }: HeaderProps) {
   return (
     <Container>
-      <Text opacity={0.9}>Bem-vindo(a) ao</Text>
-      <Text size={24} weight="700">
-        WAITER
-        <Text size={24}>APP</Text>
-      </Text>
+      {!selectedTable && (
+        <>
+          <Text opacity={0.9}>Bem-vindo(a) ao</Text>
+          <Text size={24} weight="700">
+            WAITER
+            <Text size={24}>APP</Text>
+          </Text>
+        </>
+      )}
+
+      {selectedTable && (
+        <Content>
+          <OrderHeader>
+            <Text size={24} weight="400">
+              Pedido
+            </Text>
+            <TouchableOpacity onPress={ onCancelOrder }>
+              <Text size={14} color="#D73035" weight="600">
+                cancelar pedido
+              </Text>
+            </TouchableOpacity>
+          </OrderHeader>
+          <Table>
+            <Text color="#666">Mesa { selectedTable }</Text>
+          </Table>
+        </Content>
+      )}
     </Container>
   );
 }
