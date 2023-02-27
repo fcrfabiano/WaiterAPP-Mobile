c43297f7804119e59400207c3fd4f34c03e4488a
Fabiano César <fcfabiano.cesar@gmail.com>
2023-02-27 16:41:13 -0300

    mock: change mock products

--- src/mocks/products.ts
+++ src/mocks/products.ts
@@ -3,37 +3,37 @@ export const products = [
     _id: '6372e040f52e37ef85fe2c5e',
     name: 'Pizza quatro queijos',
     description: 'Deliciosa pizza quatro queijos com borda simples',
-    imagePath: '1668472896991-quatro-queijos.png',
+    imagePath: '1668556758387-quatro-queijos.png',
     price: 40,
     ingredients: [
       {
         name: 'Mussarela',
         icon: '🧀',
-        _id: '6372e040f52e37ef85fe2c5f'
+        _id: '6372e040f52e37ef85fe2c5f',
       },
       {
         name: 'Parmesão',
         icon: '🧀',
-        _id: '6372e040f52e37ef85fe2c60'
+        _id: '6372e040f52e37ef85fe2c60',
       },
       {
         name: 'Gouda',
         icon: '🧀',
-        _id: '6372e040f52e37ef85fe2c61'
+        _id: '6372e040f52e37ef85fe2c61',
       },
       {
         name: 'Brie',
         icon: '🧀',
-        _id: '6372e040f52e37ef85fe2c62'
-      }
+        _id: '6372e040f52e37ef85fe2c62',
+      },
     ],
   },
   {
     _id: '6372e276a381106c0f854cb3',
-    name: 'Coca cola',
-    description: 'Coca cola lata geladinha topzera',
-    imagePath: '1668473462705-coca-cola.png',
-    price: 7,
+    name: 'Madero\'s Strawberry - 150ml',
+    description: 'Delicioso drink a base de Vodka, Amaretto, Limão e Morango',
+    imagePath: '1668557503628-strawberry.webp',
+    price: 27.90,
     ingredients: [],
   },
 ];
